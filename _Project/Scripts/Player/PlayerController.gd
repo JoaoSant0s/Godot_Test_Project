609cc9bdec8ec4e0d08afd8da67d832be9e580df
João Santos <ricardoelric@gmail.com>
2023-10-01 22:09:03 -0300

    (imp): making player controller better to test different cameras

--- _Project/Scripts/Player/PlayerController.gd
+++ _Project/Scripts/Player/PlayerController.gd
@@ -1,26 +1,18 @@
 extends CharacterBody3D
  
 const SPEED = 5.0
+const ANGULAR_SPEED = 0.1
 const JUMP_VELOCITY = 4.5
  
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
  
-#@onready var camera: Camera3D = $Camera3D
- 
 func _ready() -> void:
-	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	pass
- 
-func _unhandled_input(event: InputEvent) -> void:
-	if not event is InputEventMouseMotion: return
-	
-	#rotate_y(-event.relative.x * .005)
-	#camera.rotate_x(-event.relative.y * .005)
-	#camera.rotation.x = clamp(camera.rotation.x, -PI/4, PI/4)
  
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
  
@@ -32,9 +24,12 @@ func _physics_process(delta: float) -> void:
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		rotation.y -= input_dir.x * ANGULAR_SPEED
+		if input_dir.y != 0:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
