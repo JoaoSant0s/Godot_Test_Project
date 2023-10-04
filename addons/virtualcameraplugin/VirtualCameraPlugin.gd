40bf597be8cb15c8db1d6bfa5479cf0d2dea2eec
João Santos <ricardoelric@gmail.com>
2023-10-04 01:25:57 -0300

    (refactor): moving icons to exclusive folder

--- addons/virtualcameraplugin/VirtualCameraPlugin.gd
+++ addons/virtualcameraplugin/VirtualCameraPlugin.gd
@@ -7,8 +7,8 @@ const MAIN_CAMERA_NAME = "MainCamera"
 
 func _enter_tree():
 	add_autoload_singleton(AUTOLOAD_CAMERA_SERVICE, "res://addons/virtualcameraplugin/VirtualCamera/Runtime/VirtualCameraService.gd")
-	add_custom_type(MAIN_CAMERA_NAME, "Camera3D", preload("VirtualCamera/Runtime/MainCamera.gd"), preload("camera-main.svg"))
-	add_custom_type(VIRTUAL_CAMERA_NAME, "Node3D", preload("VirtualCamera/Runtime/VirtualCamera.gd"), preload("camera-virtual.svg"))
+	add_custom_type(MAIN_CAMERA_NAME, "Camera3D", preload("VirtualCamera/Runtime/MainCamera.gd"), preload("VirtualCamera/Icons/camera-main.svg"))
+	add_custom_type(VIRTUAL_CAMERA_NAME, "Node3D", preload("VirtualCamera/Runtime/VirtualCamera.gd"), preload("VirtualCamera/Icons/camera-virtual.svg"))
 	
 func _exit_tree():
 	remove_autoload_singleton(AUTOLOAD_CAMERA_SERVICE)
