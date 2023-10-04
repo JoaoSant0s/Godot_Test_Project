bfc904bb2695babab6a5466fce2b082ca4ba2f32
João Santos <ricardoelric@gmail.com>
2023-10-04 01:29:05 -0300

    (refactor): creating a method that create just components

--- addons/virtualcameraplugin/VirtualCamera/Runtime/Wrappers/UtilsCamera.gd
+++ addons/virtualcameraplugin/VirtualCamera/Runtime/Wrappers/UtilsCamera.gd
@@ -1,13 +1,19 @@
 class_name UtilsCamera
 
-enum UpdateMethods {DEFAULT_PROCESS, PHYSICS_PROCESS, DISABLED}
+enum UpdateMethods {DEFAULT_PROCESS, PHYSICS_PROCESS, DISABLED}	
 
 static func createLensComponent(virtualCamera : VirtualCamera):
 	var hasLens = virtualCamera.get_children().any(func (node): return node is LensComponent);
 	if hasLens: return
-		
-	var lensInstance = load("res://addons/virtualcameraplugin/VirtualCamera/Prefabs/LensComponent.tscn").instantiate()
-	lensInstance.name = "LensComponent"
-	virtualCamera.add_child(lensInstance);
+			
+	var lensInstance = _createComponent("LensComponent", "res://addons/virtualcameraplugin/VirtualCamera/Prefabs/LensComponent.tscn", virtualCamera) as LensComponent
+	virtualCamera.lens = lensInstance;
 	lensInstance.set_owner(virtualCamera.get_tree().edited_scene_root)
-	virtualCamera.lens = lensInstance;	
+
+
+static func _createComponent(name : String, path : String, parent):
+	var lensInstance = load(path).instantiate()
+	lensInstance.name = name
+	parent.add_child(lensInstance);
+	lensInstance.set_owner(parent.get_tree().edited_scene_root)
+	return lensInstance
