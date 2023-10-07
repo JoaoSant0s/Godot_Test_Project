9471391f98146f9c2b67576368af3e7ea3941ed6
João Santos <ricardoelric@gmail.com>
2023-10-07 01:14:26 -0300

    (imp): adding tracking mode

--- addons/virtualcameraplugin/VirtualCamera/Runtime/Components/TrackingComponent.gd
+++ addons/virtualcameraplugin/VirtualCamera/Runtime/Components/TrackingComponent.gd
@@ -1,5 +1,6 @@
 @tool
 class_name TrackingComponent extends VirtualCameraBaseComponent
 
+@export var trackingMode : TypeCameras.TrackingMode = TypeCameras.TrackingMode.HARDLOCK
 @export var target : Node3D
 @export var lookAt : Node3D
--- addons/virtualcameraplugin/VirtualCamera/Runtime/Wrappers/TypeCameras.gd
+++ addons/virtualcameraplugin/VirtualCamera/Runtime/Wrappers/TypeCameras.gd
@@ -3,3 +3,4 @@ class_name TypeCameras extends Node
 enum ProcessMethods {DEFAULT_PROCESS, PHYSICS_PROCESS, DISABLED}
 
 enum TransitionMethods {CUT}
+enum TrackingMode {NONE, HARDLOCK, FOLLOW}
