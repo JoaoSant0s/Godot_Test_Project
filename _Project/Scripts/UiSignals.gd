cef5f821c9b86ea35b1d9583113564cf14389fca
João Santos <ricardoelric@gmail.com>
2024-02-01 21:44:53 -0300

    (imp): creating close game action

--- _Project/Scripts/UiSignals.gd
+++ _Project/Scripts/UiSignals.gd
@@ -3,5 +3,5 @@ extends Node
 signal onTestSignal(oldValue : int, newValue : int)
 
 func _unhandled_input(_event: InputEvent) -> void: 
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("close_game"):
 		get_tree().quit()
