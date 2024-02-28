ca3106c07e227e09b440325e218f46204b0d7953
João Santos <ricardoelric@gmail.com>
2024-02-28 19:21:46 -0300

    (imp): removing old test scripts and scenes

--- _Project/Scripts/UiSignals.gd
+++ _Project/Scripts/UiSignals.gd
@@ -1,7 +1,5 @@
 extends Node
 
-signal onTestSignal(oldValue : int, newValue : int)
-
 func _unhandled_input(_event: InputEvent) -> void: 
 	if Input.is_action_just_pressed("close_game"):
 		get_tree().quit()
