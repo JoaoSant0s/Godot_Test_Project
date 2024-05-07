fdf4a9a079485c66518a26e2532aa046254e7df4
João Santos <ricardoelric@gmail.com>
2024-05-07 19:35:52 -0300

    (fix): web build break during the quit

--- _Project/Scripts/UiSignals.gd
+++ _Project/Scripts/UiSignals.gd
@@ -1,5 +1,5 @@
 extends Node
 
 func _unhandled_input(_event: InputEvent) -> void: 
-	if Input.is_action_just_pressed("close_game"):
+	if Input.is_action_just_pressed("close_game") and OS.get_name() != "Web":
 		get_tree().quit()
