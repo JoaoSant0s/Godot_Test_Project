523cb04189511029b5c18e49396faa228c48a0ed
João Santos <ricardoelric@gmail.com>
2024-06-03 22:08:46 -0300

    (hold): removing basic references for damped sprint method to work in this feature later

--- addons/virtual_camera/Runtime/Objects/Transitions/CameraTransitionSimulator.gd
+++ addons/virtual_camera/Runtime/Objects/Transitions/CameraTransitionSimulator.gd
@@ -3,26 +3,14 @@ class_name  CameraTransitionSimulator
 var _previousCamera : VirtualCamera
 var _nextCamera : VirtualCamera
 var _transitionConfig : TransitionMethodConfig
-var _dampedSprintMotion : tDampedSpringMotionParams
 
 var timedElapsed : float
-#var position : Vector3
-#var velocity : Vector3
 
 func _init(previousCamera: VirtualCamera, nextCamera : VirtualCamera, transitionConfig : TransitionMethodConfig):
 	_previousCamera = previousCamera
 	_nextCamera = nextCamera
 	_transitionConfig = transitionConfig
-	timedElapsed = 0
-	#velocity = Vector3.ZERO
-	#
-	#if previousCamera:
-		#position = previousCamera.global_position
-	#elif nextCamera:
-		#position = nextCamera.global_position
-	#else:
-		#position = Vector3.ZERO	
-	#_dampedSprintMotion = tDampedSpringMotionParams.new()
+	timedElapsed = 0	
 # Start Override Region
 
 func pre_update(delta : float):
@@ -49,19 +37,9 @@ func build_position(delta : float):
 	
 	if _nextCamera.tracking.target:
 		nextCameraPosition = _nextCamera.tracking.get_position()
-	
-	# TODO Make here the damping transition effect
-	#dampedSpringMotion.CalcDampedSpringMotionParams(_dampedSprintMotion, delta, 1,  1)
-	#var result = dampedSpringMotion.UpdateDampedSpringMotionVector(velocity, position, nextCameraPosition, _dampedSprintMotion)
-	#var result = dampedSpringMotion.UpdateDampedSpringMotion(velocity.y, position.y, nextCameraPosition.y, _dampedSprintMotion)
-	#position.x = nextCameraPosition.x
-	#position.z = nextCameraPosition.z
-	#position.y = result.pPos
-	#velocity.y = result.pVel
-	#_nextCamera.global_position = position
-	_nextCamera.global_position = nextCameraPosition
 
-	_nextCamera.global_position += _nextCamera.tracking.get_local_position_control()
+	nextCameraPosition += _nextCamera.tracking.get_local_position_control()
+	_nextCamera.global_position = nextCameraPosition
 
 func build_rotation():
 	if _nextCamera.tracking.target and _nextCamera.tracking.is_rotation_control_same_as_follow_target():
